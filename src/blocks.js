5fe12531023fca2b4e99bd919c653ea51749d8e8
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-08-31 14:19:20 +0900

    add meta box

--- src/blocks.js
+++ src/blocks.js
@@ -9,4 +9,130 @@
  * Webpack is compiling as the input file.
  */
 
-import './block/block.js';
+//import './block/block.js';
+
+/**
+ * WordPress dependencies
+ */
+
+const { Fragment } = wp.element;
+const { PluginSidebar, PluginSidebarMoreMenuItem, PluginPrePublishPanel } = wp.editPost;
+const { registerPlugin } = wp.plugins;
+const { __ } = wp.i18n;
+const { compose } = wp.compose;
+const { withSelect, withDispatch } = wp.data;
+const { Panel, PanelBody, PanelRow, SelectControl } = wp.components;
+const { PluginDocumentSettingPanel } = wp.editPost;
+
+const postmeta_key = 'post_relationship';
+
+const PostSelector = ( ( { postId, posts, setPostId } ) => {
+	return (
+		<SelectControl
+			name={ 'post-relationship' }
+			label={ __( 'Writer', 'post-relationship' ) }
+			value={ postId }
+			options={ [
+				{
+					label: __( 'None' ),
+					value: null,
+				},
+				...posts.map( ( { id, title } ) => {
+					return {
+						label: title.rendered,
+						value: id,
+					};
+				} ) ] }
+			onChange={ ( id ) => {
+				setPostId( { id } );
+			} }
+		/>
+	);
+} );
+
+const AuthorSelector = compose( [
+	withSelect(
+		( select ) => {
+			const { getEntityRecords } = select( 'core' );
+			const { getEditedPostAttribute } = select( 'core/editor' );
+			const s = getEntityRecords( 'postType', 'post', { per_page: - 1 } ) || [];
+			const meta = getEditedPostAttribute( 'meta' ) || {};
+			return {
+				posts: s,
+				postId: meta[ postmeta_key ]
+			};
+
+		}
+	),
+	withDispatch( ( dispatch ) => {
+		return {
+			setPostId: ( { id } ) =>
+				dispatch( 'core/editor' ).editPost(
+					{
+						meta: { [ postmeta_key ]: parseInt( id ) }
+					}
+				),
+		};
+	} )
+] )
+( PostSelector );
+
+const PluginPanel = () => (
+	<PanelBody
+		title="Post Relationship"
+		initialOpen={ true }
+	>
+		<PanelRow>
+			<AuthorSelector />
+		</PanelRow>
+	</PanelBody>
+);
+
+const Component = () => (
+	<Fragment>
+		<PluginSidebarMoreMenuItem target={ 'post-relationship' }>
+			{ __( 'Writer' ) }
+		</PluginSidebarMoreMenuItem>
+		<PluginSidebar
+			name={ 'post-relationship' }
+			title={ __( 'Writer', 'post-relationship' ) }
+		>
+			<Panel>
+				<PluginPanel />
+			</Panel>
+
+		</PluginSidebar>
+
+		<PluginPrePublishPanel
+			title="Post Relationship"
+			initialOpen={ true }>
+			<PanelRow>
+				<AuthorSelector />
+			</PanelRow>
+		</PluginPrePublishPanel>
+	</Fragment>
+);
+
+
+if ( PluginDocumentSettingPanel ) {
+	registerPlugin( 'post-relationship', {
+		icon: 'admin-users',
+		render: () => (
+			<Fragment>
+				<PluginDocumentSettingPanel className="my-document-setting-plugin" title="My Panel">
+					<AuthorSelector />
+				</PluginDocumentSettingPanel>
+				<Component />
+			</Fragment>
+
+		)
+	} )
+}else {
+
+	registerPlugin( 'post-relationship', {
+		icon: 'admin-users',
+		render: Component,
+	} );
+}
+
+
--- src/init.php
+++ src/init.php
@@ -40,50 +40,27 @@ function my_guten_plugin_cgb_block_assets() { // phpcs:ignore
 	wp_register_script(
 		'my_guten_plugin-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
+		array(
+			'wp-blocks',
+			'wp-components',
+			'wp-data',
+			'wp-element',
+			'wp-editor',
+			'wp-edit-post',
+			'wp-i18n',
+		), // Dependencies, defined above.
 		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
 
-	// Register block editor styles for backend.
-	wp_register_style(
-		'my_guten_plugin-cgb-block-editor-css', // Handle.
-		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
-		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
-	);
-
+	wp_enqueue_script('my_guten_plugin-cgb-block-js');
 	// WP Localized globals. Use dynamic PHP stuff in JavaScript via `cgbGlobal` object.
-	wp_localize_script(
-		'my_guten_plugin-cgb-block-js',
-		'cgbGlobal', // Array containing dynamic data for a JS Global.
-		[
-			'pluginDirPath' => plugin_dir_path( __DIR__ ),
-			'pluginDirUrl'  => plugin_dir_url( __DIR__ ),
-			// Add more data here that you want to access from `cgbGlobal` object.
-		]
-	);
 
-	/**
-	 * Register Gutenberg block on server-side.
-	 *
-	 * Register the block on server-side to ensure that the block
-	 * scripts and styles for both frontend and backend are
-	 * enqueued when the editor loads.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
-	 * @since 1.16.0
-	 */
-	register_block_type(
-		'cgb/block-my-guten-plugin', array(
-			// Enqueue blocks.style.build.css on both frontend & backend.
-			'style'         => 'my_guten_plugin-cgb-style-css',
-			// Enqueue blocks.build.js in the editor only.
-			'editor_script' => 'my_guten_plugin-cgb-block-js',
-			// Enqueue blocks.editor.build.css in the editor only.
-			'editor_style'  => 'my_guten_plugin-cgb-block-editor-css',
-		)
-	);
+	register_post_meta( '', 'post_relationship', array(
+		'show_in_rest' => true,
+		'single'       => true,
+		'type'         => 'number',
+	) );
 }
 
 // Hook: Block assets.
